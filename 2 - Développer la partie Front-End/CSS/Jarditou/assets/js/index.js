4c1b7e9aa8093fe0fbdd70065602a3991455f159
Julien-Broyard <broyard.dev@gmail.com>
2020-02-10 13:33:14 +0100

    :fire: Add finished Jarditou

--- 2 - Développer la partie Front-End/CSS/Jarditou/assets/js/index.js
+++ 2 - Développer la partie Front-End/CSS/Jarditou/assets/js/index.js
@@ -1,5 +1,52 @@
 $(document).ready(function() {
-  $(".sidenav").sidenav();
-  $(".parallax").parallax();
+  $(".datepicker").datepicker({
+    format: "dd/mm/yyyy",
+    i18n: {
+      months: [
+        "Janvier",
+        "Février",
+        "Mars",
+        "Avril",
+        "Mai",
+        "Juin",
+        "Juillet",
+        "Août",
+        "Septembre",
+        "Octobre",
+        "Novembre",
+        "Décembre"
+      ],
+      monthsShort: [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Avr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Aou",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ],
+      weekdays: [
+        "Dimanche",
+        "Lundi",
+        "Mardi",
+        "Mercredi",
+        "Jeudi",
+        "Vendredi",
+        "Samedi"
+      ],
+      weekdaysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+      weekdaysAbbrev: ["D", "L", "M", "M", "J", "V", "S"],
+      close: "Fermer",
+      cancel: "Annuler"
+    }
+  });
   $(".materialboxed").materialbox();
+  $(".parallax").parallax();
+  $("select").formSelect();
+  $(".sidenav").sidenav();
 });
