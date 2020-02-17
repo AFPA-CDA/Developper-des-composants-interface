47fab71612449513f357bf194d3c401b60432621
Julien-Broyard <broyard.dev@gmail.com>
2020-02-17 16:54:21 +0100

    :sparkles: Upgrade CSS

--- 2 - Développer la partie Front-End/Evaluation/assets/js/form.js
+++ 2 - Développer la partie Front-End/Evaluation/assets/js/form.js
@@ -5,7 +5,7 @@ function verify() {
   const hasErrors = [];
 
   // Here lies all the regex used for this form
-  const isAlpha = /^[\wÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ\'’\s-]+$/i;
+  const isAlpha = /^[\wÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'’\s-]+$/i;
   const isDate = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/g;
   const isDangerous = /[<>&]/g;
   const isEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g;
