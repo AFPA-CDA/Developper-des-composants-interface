7d40329e294a162e39694492a4bc78cef69eba6e
Julien-Broyard <broyard.dev@gmail.com>
2020-04-23 17:52:38 +0200

    :sparkles: Add Scanner input validation + menu loop

--- 4 - Développer une application desktop/Développer en Java/src/com/afpa/Main.java
+++ 4 - Développer une application desktop/Développer en Java/src/com/afpa/Main.java
@@ -7,9 +7,51 @@ import com.afpa.operator.*;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // Creates the header string
-        String header = new StringBuilder()
+        while (true) {
+            // Gets the Menu Header
+            String header = header();
+
+            // Prints the header string
+            System.out.println(header);
+            System.out.println("Entrez le numéro de l'exercice que vous voulez tester.");
+
+            // While the user don't give a valid integer
+            while (!scan.hasNextInt()) {
+                // Prints an error message
+                System.out.println("Vous devez donner un nombre positif.");
+                // Finds and returns the next scan input
+                scan.next();
+            }
+
+            // Gets the user input to choose the exercise
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 0 -> System.exit(0);
+                case 1 -> Circle.exec();
+                case 2 -> Swap.exec();
+                case 3 -> Adder.exec();
+                case 4 -> Divider.exec();
+                case 5 -> Temperature.exec();
+                case 6 -> Average.exec();
+                case 7 -> Seconds.exec();
+                case 8 -> Ascii.exec();
+                case 9 -> Even.exec();
+                case 10 -> Age.exec();
+                case 11 -> Calculator.exec();
+                case 12 -> Discount.exec();
+                default -> System.out.println("L'exercice demandé n'existe pas.");
+            }
+        }
+    }
+
+    public static String header() {
+        return new StringBuilder()
+                .append("\n")
+                .append("0: Exit\n")
                 .append("---------- Syntaxe ----------\n")
                 .append("1: Circle\n")
                 .append("2: Swap\n")
@@ -27,31 +69,5 @@ public class Main {
                 .append("\t11: Calculator\n")
                 .append("\t12: Discount\n")
                 .toString();
-
-        // Prints the header string
-        System.out.println(header);
-        System.out.println("Entrez le nombre de l'exercice que vous voulez tester.");
-
-        // Creates a Scanner
-        Scanner scan = new Scanner(System.in);
-
-        // Gets the user input to choose the exercice
-        int choice = scan.nextInt();
-
-        switch (choice) {
-            case 1 -> Circle.exec();
-            case 2 -> Swap.exec();
-            case 3 -> Adder.exec();
-            case 4 -> Divider.exec();
-            case 5 -> Temperature.exec();
-            case 6 -> Average.exec();
-            case 7 -> Seconds.exec();
-            case 8 -> Ascii.exec();
-            case 9 -> Even.exec();
-            case 10 -> Age.exec();
-            case 11 -> Calculator.exec();
-            case 12 -> Discount.exec();
-            default -> System.out.println("L'exercice demandé n'existe pas.");
-        }
     }
 }
