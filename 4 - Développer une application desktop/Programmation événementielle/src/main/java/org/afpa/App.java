2151575280a4596faf520cdc00af057d2f2ee56a
Julien-Broyard <broyard.dev@gmail.com>
2020-05-07 14:55:11 +0200

    :sparkles: Add first JavaFX exercice

---
+++ 4 - Développer une application desktop/Programmation événementielle/src/main/java/org/afpa/App.java
@@ -0,0 +1,57 @@
+package org.afpa;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Entrypoint of the application
+ *
+ * @author Julien
+ * @version 1.0.0
+ * @see Application
+ * @since 1.0.0
+ */
+public class App extends Application {
+    private static Scene scene;
+
+    /**
+     * @param stage The stage to start with
+     * @throws IOException If the function can't read the file
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("adder"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param fxml The fxml file to load
+     * @throws IOException If the function can't read the file
+     */
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    /**
+     * @param fxml The fxml file to load
+     * @return The loaded file
+     * @throws IOException If the function can't read the file
+     */
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    /**
+     * @param args Args given from the command line
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ 4 - Développer une application desktop/Programmation événementielle/src/main/java/org/afpa/controllers/Adder.java
@@ -0,0 +1,66 @@
+package org.afpa.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Julien
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Adder implements Initializable {
+    @FXML
+    private TextArea TextAreaAdder;
+    private int result;
+    private boolean isFirst;
+
+    /**
+     * @see Initializable
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.result = 0;
+        this.isFirst = true;
+    }
+
+    /**
+     * Returns the result of the sum
+     */
+    public void add() {
+        this.TextAreaAdder.appendText(" = " + this.result);
+        this.isFirst = true;
+    }
+
+    /**
+     * Clear the text area's text and resets the result
+     */
+    public void clear() {
+        this.result = 0;
+        this.TextAreaAdder.clear();
+        this.TextAreaAdder.appendText(String.valueOf(this.result));
+    }
+
+    /**
+     * Adds the numbers on the
+     * @param actionEvent The event on the button click
+     */
+    public void numberButtonClick(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        String value = node.getUserData().toString();
+
+        if (this.isFirst) {
+            this.TextAreaAdder.appendText(value);
+            this.isFirst = false;
+        } else {
+            this.TextAreaAdder.appendText(" + " + value);
+        }
+
+        this.result += Integer.parseInt(value);
+    }
+}
